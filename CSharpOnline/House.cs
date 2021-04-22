c56aad38bf5c7666bc85760788bd68ef37ed6ba7
Daniel Simmons <bluefolderredpen@gmail.com>
2021-04-22 05:21:01 -0500

    Lesson 4 My Application Breakdown

---
+++ CSharpOnline/House.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOnline
+{
+    class House
+    {
+        // Attributes of the class
+        int _windowSize;
+        string _foundation;
+        string _roofType;
+        string _doorColor;
+
+        /*
+         * This method will print out
+         * about what the door does.
+         */
+        void DoorOpenClose()
+        {
+            Console.WriteLine("The door opens");
+        }
+
+
+
+    }
+}
